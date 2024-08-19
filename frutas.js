aeffa639af94f2981f4b1db41ddd0e713a02f1f5
Murilo <129951122+demonlishing@users.noreply.github.com>
2024-08-19 09:03:27 -0300

    Create frutas.js

---
+++ frutas.js
@@ -0,0 +1,10 @@
+const frutas = ["maça","pera","banana", "limão", "melancia"]
+
+let frutasM = []
+
+for(let i = 0; i < frutas.length; i ++){
+    frutasM.push(frutas[i].toUpperCase());
+}
+  
+
+console.log(frutasM)
